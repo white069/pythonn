d38ccd3bd218f360434ab548cca8ab050d9eac3f
Alexius <freddkolo6@gmail.com>
2025-04-17 13:16:19 +0200

    i forgor to commit" ovo je rebejsovano

--- week5/zbirka.py
+++ week5/zbirka.py
@@ -276,6 +276,7 @@ def ispisati(x):
 # print(f"{dist:.2f}")
 #===================================================================    
     
+<<<<<<< HEAD
     
 #===================================================================
 #Zadatak  19
@@ -401,6 +402,372 @@ def ispisati(x):
 # Излаз
 # da
 #sta oni hocu je da jednu promenljivu stavim na false i ako neki uslov ispunjava postavim je na true
+=======
+#===================================================================
+#Zadatak  12
+# Задатак: Позиција највећег угла троугла
+# Дата су два угла разностраничног троугла у степенима, минутима и секундама. Одредити који од углова
+# троугла је највећи, први по редоследу уноса, други по редоследу уноса или трећи, одређен у програму.
+# Улаз: У свакој од шест линија стандардног улаза налази се по један цео број. Бројеви редом представљају
+# степене, минуте (мање од 60) и секунде (мање од 60), прво једног, па другог угла троугла. Збир дата два угла
+# је мањи од 180 степени.
+# Излаз: У једној линији стандардног излаза исписати број 1, 2 или 3.
+# Пример
+# Улаз
+# 75
+# 30
+# 14
+# 23
+# 15
+# 45
+# Излаз
+# 3
+
+# stp1=int(input('stepen prvog ugla '))
+# min1=int(input('minut prvog ugla '))
+# sek1=int(input('sekund prvog ugla '))
+# stp2=int(input('stepen drugog ugla '))
+# min2=int(input('minut drugog ugla '))
+# sek2=int(input('sekund drugog ugla '))
+
+# stp3=179-(stp1+stp2)
+# min12=min1+min2
+# if min12>=60:
+#    min12= 118-min12
+#    stp3-=1
+# min3=59-min12
+# sek12=sek1+sek2
+# if sek12>=60:
+#     sek12=118-sek12
+#     min3-=1
+# sek3=59-sek12
+# def jednakost(min1,min2,sek1,sek2):
+#     if min1>min2:
+#         print(1)
+#     elif min2>min1:
+#         print(2)
+#     else:
+#         if sek1>sek2:
+#             print(1)
+#         else:
+#             print(2)
+# def jednakost1(min1,min2,sek1,sek2):
+#     if min1>min2:
+#         print(2)
+#     elif min2>min1:
+#         print(3)
+#     else:
+#         if sek1>sek2:
+#             print(2)
+#         else:
+#             print(3)
+
+
+# if stp1>stp2 and stp1>stp3:
+#     print(1)
+# elif stp2>stp1 and stp2>stp3:
+#     print(2)
+# elif stp3>stp1 and stp3>stp2:
+#     print(3)
+# elif stp1>stp2 and stp1==stp3:
+#     if min1>min3:
+#         print(1)
+#     elif min3>min1:
+#         print(3)
+#     else:
+#         if sek1>sek3:
+#             print(1)
+#         else:
+#             print(3)
+# elif stp1>stp3 and stp1==stp2:
+#     jednakost(min1,min2,sek1,sek2)
+# else:
+#     jednakost1(min2,min3,sek2,sek3)
+# sada kada sam ga uradio, vidim da je ovo najgori moguci kod ikada stvoren. 
+# Umesto da mi fja radi jedan posao on pravo ide ka onome sto treba, sto zauzvrat nema fleksibilnost
+# mogao sam kao da napravim da fja vraca 1 ako je prva veca, 2 ako je druga pa preko toga da kazem sta je vece
+# ili jos bolje mogao sam da pretvorim sve u sekunde pa da to uporedim
+#===================================================================   
+    
+#===================================================================
+#Zadatak  13
+# Задатак: Најближи просеку
+# Написати програм који за четири дата броја одређује који је од та четри броја најближи аритметичкој средини датих бројева (ако су два броја једнако близу исписати први).
+# Улаз: У четири линије стандардног улаза налазе се четири реална броја.
+# Излаз: У једној линији стандардног излаза приказати један од унетих реалних бројева, на две децимале,
+# најближи њиховој аритметичкој средини.
+# Пример
+# Улаз
+# 24.3
+# 20.2
+# 23.5
+# 22.7
+# Излаз
+# 22.70
+
+# a=float(input('prvi broj '))
+# b=float(input('drugi broj '))
+# c=float(input('treci broj '))
+# d=float(input('cetvrti broj '))
+# sredina=(a+b+c+d)/4
+# a1=abs(sredina-a)
+# b1=abs(sredina-b)
+# c1=abs(sredina-c)
+# d1=abs(sredina-d)
+# najblizi=min(a1,b1,c1,d1)
+# if najblizi==a1:
+#     print(format(a,'.2f'))
+# elif najblizi==b1:
+#     print(format(b,'.2f'))
+# elif najblizi==c1:
+#     print(format(c,'.2f'))
+# else:
+#     print(format(d,'.2f'))
+#===================================================================
+
+#===================================================================
+#Zadatak  14
+# Задатак: Први и други максимум
+# Написати програм којим се одређују највећа два различита броја од пет датих целих бројева.
+# Улаз: На станадардном улазу налазе се 5 целих бројева, сваки у посебној линији.
+# Излаз: Прва линија стандарног излаза садржи највећи број од датих 5 бројева. Друга линија стандардног
+# излаза садржи други по величини цео број од датих 5 бројева. Aко су сви унети бројеви једнаки друга линија
+# треба да садржи само знак ‘-’.
+# Пример 1
+# Улаз
+# 2
+# 7
+# -4
+# 7
+# 3
+# Излаз
+# 7
+# 3
+# Пример 2
+# Улаз
+# 12
+# 12
+# 12
+# 12
+# 12
+# Излаз
+# 12
+
+# brojevi=list(map(int,input('5 brojeva: ').split()))
+# maks1=max(brojevi)
+# print(maks1)
+# skup=set(brojevi)
+# skup.remove(maks1)
+# if len(skup)==0:
+#     print('-')
+# else: 
+#     maks2=max(skup)
+#     print(maks2)
+#===================================================================
+
+
+#===================================================================
+#Zadatak  15
+# Задатак: Аутомобил у вођству
+# Три аутомобила крећу са стартне позиције у тренуцима 0 ≤ t1 ≤ t2 ≤ t3 константним брзинама v1, v2 и
+# v3. Приказати стартне бројеве аутомобила који су на водећој позицији у тренутку t (t ≥ 0). Ако ни један
+# аутомобил још није кренуо, сва три су у водећој позицији.
+# Улаз: Са стандардног улаза учитава се седам целих бројева, сваки у засебној линији t1, v1, t2, v2, t3, v3, t где
+# t1, t2, t3, t представљају времена у секундама, а v1, v2, v3 брзине у метрима по секунди.
+# Излаз: На стандардни излаз исписати један или више целих бројева (сваки у посебном реду) који представљају редне бројеве аутомобила (1, 2 или 3) који су на водећој позицији. Ако је више аутомобила истовремено
+# у вођству, њихове редне бројеве исписати у растућем редоследу.
+# Пример
+# Улаз
+# 10
+# 5
+# 5
+# 4
+# 0
+# 2
+# 30
+# Излаз
+# 1
+# 2
+
+# t1=int(input('startna pozicija auta u trenutku: '))
+# v1=int(input('brzina tog auta: '))
+# t2=int(input('startna pozicija drugog auta: '))
+# v2=int(input('brzina tog auta: '))
+# t3=int(input('startna pozicija treceg auta: '))
+# v3=int(input('brzina tog auta: '))
+# t=int(input('zeljeno vreme posmatranja: '))
+
+# s1 = (t - t1) * v1 if t >= t1 else 0
+# s2 = (t - t2) * v2 if t >= t2 else 0
+# s3 = (t - t3) * v3 if t >= t3 else 0
+# najvise = max(s1, s2, s3)
+
+# if s1 == najvise:
+#     print(1)
+# if s2 == najvise:
+#     print(2)
+# if s3 == najvise:
+#     print(3)
+#===================================================================
+
+
+#===================================================================
+#Zadatak  16
+# Задатак: Јутарње температуре
+# Дате су јутарње температуре за 4 дана T1, T2, T3 и T4. Одредити број дана од та 4 дана када је јутарња
+# температура била испод просечне температуре за та 4 дана.
+# Улаз: Ca стандардног улаза се учитавају четири реална броја (сваки је у посебној линији) који представљају
+# јутарње температуре за четири дана.
+# Излаз: У првој линији стандардног улаза исписује се просечна температура за та четири дана, заокружена
+# на две децимале.
+# У другој линији стандардног излаза исписује се број дана када је температура била испод просечне.
+# Пример
+# Улаз
+# 27.3
+# 20.2
+# 23.5
+# 21.8
+# Излаз
+# 23.20
+# 2
+# t1=float(input('temperatura tog dana: '))
+# t2=float(input('temp nar dan '))
+# t3=float(input('temp nar nar '))
+# t4=float(input('temp nar nar nar '))
+# sredina=(t1+t2+t3+t4)/4
+# print(format(sredina,'.2f'))
+# i=0
+# if t1<sredina:
+#     i+=1
+# if t2<sredina:
+#     i+=1
+# if t3<sredina:
+#     i+=1
+# if t4<sredina:
+#     i+=1
+# print(i)
+#===================================================================
+
+#===================================================================
+#Zadatak  17
+# Задатак: Квалификациони праг
+# Ако се знају поени 4 такмичара и квалификациони праг на такмичењу, одредити колико такмичара је освојило довољно поена и квалификовало се у следећи круг такмичења, а колики је просек поена оних који се
+# нису квалификовали.
+# Улаз: У пет линија стандардног улаза налазе се пет целих бројева из интервала [0, 100], сваки број у посебној
+# линији. Прва четири броја представљају освојене поене такмичара, а пети број је квалификациони праг.
+# Излаз: На стандардном излазу исписује се:
+# • у првој линији број такмичара који су се квалификовали,
+# • у другој линији просек поена (на две децимале) за групу такмичара која се није квалификовала, а у
+# случају да су се сви квалификовали у другој линији исписати знак ‘-’.
+# Пример
+# Улаз
+# 0
+# 30
+# 2
+# 99
+# 30
+# Излаз
+# 2
+# 1.00
+# poen1=int(input('poeni ostvareni od strane prvog takmicara:'))
+# poeni2=int(input('-||-:'))
+# poeni3=int(input('-||-:'))
+# poeni4=int(input('-\\-:'))
+# kvalf=int(input('kvalifikacioni poeni:'))
+# i=0
+# neuspeli=[]
+# g=0
+# if poen1>=kvalf:
+#     i+=1
+# else:
+#     neuspeli.append(poen1)
+#     g+=1
+# if poeni2>=kvalf:
+#     i+=1
+# else:
+#     neuspeli.append(poeni2)
+#     g+=1
+# if poeni3>=kvalf:
+#     i+=1
+# else:
+#     neuspeli.append(poeni3)
+#     g+=1
+# if poeni4>=kvalf:
+#     i+=1
+# else:
+#     neuspeli.append(poeni4)
+#     g+=1
+# print(i)
+# sredina=sum(neuspeli)/g
+# print(format(sredina,'.2f'))
+#===================================================================
+
+#===================================================================
+#Zadatak  18
+# Задатак: Претицање
+# Три аутомобила стартних бројева 1, 2, 3 крећу са исте стартне позиције редом у тренуцима t1, t2, t3 (0 ≤ t1 <
+# t2 < t3), и крећу се константним брзинама v1, v2, v3. Написати програм којим одређујемо тренутак кад се
+# деси последње претицање, или ако нема претицања исписати текст nema.
+# Улаз: Са стандардног улаза учитава се 6 реалних бројева. Прва линија садржи бројеве t1, v1, друга бројеве
+# t2, v2, а трећа бројеве t3, v3.
+# Излаз: На стандардни излаз исписати један реалан број који представља време последњег претицања, заокружен на две децимале или текст nema ако претицања није било.
+# Пример 1
+# Улаз
+# 1.6 20
+# 2 12
+# 7.4 2.1
+# Излаз
+# nema
+# Пример 2
+# Улаз
+# 6.3 9
+# 15 18
+# 19 13
+# Излаз
+# 47.57
+
+
+# t1=int(input('vreme kretanja prvog vozila:'))
+# v1=int(input('brzina kretanja prvog vozila:'))
+# t2=int(input('vreme kretanja drugog vozila:'))
+# v2=int(input('brzina kretanja drugog vozila:'))
+# t3=int(input('vreme kretanja treceg vozila:'))
+# v3=int(input('brzina kretanja treceg vozila:'))
+# bilo_preticanja = False # da li je pronađeno jedno preticanje
+# t_poslednjeg_preticanja = 0 # vreme poslednjeg preticanja
+
+# # proveravamo da li drugi automobil može preteći prvi
+# if v2 > v1:
+# t_preticanja = (v2*t2 - v1*t1) / (v2 - v1)
+# if not bilo_preticanja or t_preticanja > t_poslednjeg_preticanja:
+# t_poslednjeg_preticanja = t_preticanja
+# bilo_preticanja = True
+# # proveravamo da li treći automobil moze preteći prvi
+# if v3 > v1:
+# t_preticanja = (v3*t3 - v1*t1) / (v3 - v1)
+# if not bilo_preticanja or t_preticanja > t_poslednjeg_preticanja:
+# t_poslednjeg_preticanja = t_preticanja
+# bilo_preticanja = True
+# # proveravamo da li treći automobil moze preteći drugi
+# if v3 > v2:
+# t_preticanja = (v3*t3 - v2*t2) / (v3 - v2)
+# if not bilo_preticanja or t_preticanja > t_poslednjeg_preticanja:
+# t_poslednjeg_preticanja = t_preticanja
+# bilo_preticanja = True
+# # ako je bilo preticanja, prijavljujemo vreme poslednjeg preticanja
+# if bilo_preticanja:
+# print(format(t_poslednjeg_preticanja, '.2f'))
+# else:
+# print(”nema”)
+#===================================================================
+
+
+
+
+
+
+
+>>>>>>> a0fa89d (i forgor to commit")
 
 
 
@@ -418,6 +785,7 @@ def ispisati(x):
 
 
 
+<<<<<<< HEAD
 #===================================================================    
  
     
@@ -458,6 +826,13 @@ def ispisati(x):
  
     
  
+=======
+    
+    
+    
+    
+    
+>>>>>>> a0fa89d (i forgor to commit")
     
     
     
